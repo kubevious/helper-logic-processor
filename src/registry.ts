33068349437e6dc383fe43357059d2330238125d
Ruben Hakopian <ruben.hakopian@gmail.com>
2021-02-02 16:12:59 -0800

    added Id

--- src/registry.ts
+++ src/registry.ts
@@ -7,6 +7,36 @@ export interface IConcreteRegistry
 
 export interface IConcreteItem
 {
-    id: any
+    id: ItemId
     config: any
+}
+
+export interface ItemId {
+    infra: string,
+    api: string,
+    kind: string,
+    namespace?: string, 
+    name: string
+};
+export interface K8sConfig {
+    apiVersion: string
+    kind: string
+    metadata: {
+        name: string
+        namespace?: string
+    }
+}
+
+export function extractK8sConfigId(config: K8sConfig) : ItemId
+{
+    let itemId : ItemId = {
+        infra: 'k8s',
+        api: config.apiVersion.split('/')[0],
+        kind: config.kind,
+        name: config.metadata.name
+    }
+    if (config.metadata.namespace) {
+        itemId.namespace = config.metadata.namespace!;
+    }
+    return itemId;
 }
