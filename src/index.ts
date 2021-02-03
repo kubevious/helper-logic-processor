53f59d2c4bf40f75372e0a7ea79d19e9064522d9
Ruben Hakopian <ruben.hakopian@gmail.com>
2021-02-02 16:16:52 -0800

    added exports

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { LogicProcessor } from './processor'
 export { LogicItem } from './item'
 
 export { IConcreteRegistry, IConcreteItem } from './registry'
+export { ItemId, K8sConfig, extractK8sConfigId } from './registry'
