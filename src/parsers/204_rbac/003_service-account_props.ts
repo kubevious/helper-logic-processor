08736235501cd788b5e92119690461fa56452a8f
Ruben Hakopian <ruben.hakopian@gmail.com>
2021-09-03 22:49:47 -0700

    service account role matrix

--- src/parsers/204_rbac/003_service-account_props.ts
+++ src/parsers/204_rbac/003_service-account_props.ts
@@ -9,46 +9,19 @@ export default K8sParser<ServiceAccount, LogicServiceAccountRuntime>()
     })
     .handler(({ logger, scope, config, item, metadata, namespace, runtime, helpers }) => {
 
+        runtime.rules = helpers.roles.makeRulesMap();
 
-        // if (config.roleRef)
-        // {
-        //     const roleDn = helpers.k8s.makeDn(namespace || null, config.apiVersion!, config.roleRef.kind, config.roleRef.name);
-        //     const role = item.link('role', roleDn);
-        //     if (role)
-        //     {
-        //         runtime.rules = (<LogicRoleRuntime>role.runtime).rules;
-        //     }
-        // }
+        const bindingItems = item.resolveSourceLinkItems('subject');
+        for(let bindingItem of bindingItems)
+        {
+            runtime.rules = helpers.roles.combineRulesMap(
+                runtime.rules,
+                (<LogicRoleBindingRuntime>bindingItem.runtime).rules);
+        } 
 
-        // if (!runtime.rules) {
-        //     runtime.rules = helpers.roles.makeRulesMap();
-        // }
+        runtime.rules = helpers.roles.optimizeRulesMap(runtime.rules);
 
-        // item.addProperties(helpers.roles.buildRoleMatrixProps(runtime.rules));
-
-        // for(let subjectRef of (config.subjects || []))
-        // {
-        //     processSubject(subjectRef);
-        // }
-        
-        // /*** HELPERS ***/
-        // function processSubject(subjectRef: Subject)
-        // {
-        //     if (subjectRef.kind == 'ServiceAccount')
-        //     {
-        //         const targetNamespace = subjectRef.namespace || namespace || null;
-        //         const subjectDn = helpers.k8s.makeDn(targetNamespace, 'v1', subjectRef.kind, subjectRef.name);
-
-        //         const linkNamingParts = [subjectRef.kind];
-        //         if (targetNamespace) {
-        //             linkNamingParts.push(targetNamespace);
-        //         }
-        //         linkNamingParts.push(subjectRef.name);
-        //         const linkNaming = linkNamingParts.join('_');
-
-        //         item.link('subject', subjectDn, linkNaming);
-        //     }
-        // }
+        item.addProperties(helpers.roles.buildRoleMatrixProps(runtime.rules));
 
     })
     ;
