772fc5f5c3f92b54d22efc9af87487adee5d3422
Ruben Hakopian <ruben.hakopian@gmail.com>
2021-09-13 09:22:02 -0700

    fixes to dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
+
 export { LogicProcessor } from './processor'
 export { LogicItem } from './item'
 
 export { IConcreteRegistry, IConcreteItem } from './registry'
 export { ItemId, K8sConfig, extractK8sConfigId } from './registry'
+
+export { RegistryState } from '@kubevious/helpers/dist/registry-state';
+export { RegistryBundleState } from '@kubevious/helpers/dist/registry-bundle-state';
