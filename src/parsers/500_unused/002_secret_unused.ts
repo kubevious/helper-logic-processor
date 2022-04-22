f735d6fe97c5c9ae417610ad84ca0b2866a44b3a
Ruben Hakopian <ruben.hakopian@gmail.com>
2022-04-21 20:44:13 -0700

    unused helm secrets

--- src/parsers/500_unused/002_secret_unused.ts
+++ src/parsers/500_unused/002_secret_unused.ts
@@ -2,12 +2,17 @@ import _ from 'the-lodash';
 import { K8sParser } from '../../parser-builder';
 import { ValidatorID } from '@kubevious/entity-meta';
 import { LogicLinkKind } from '../../logic/link-kind';
+import { Secret } from 'kubernetes-types/core/v1';
 
-export default K8sParser()
+export default K8sParser<Secret>()
     .target({
         kind: "Secret"
     })
-    .handler(({ item }) => {
+    .handler(({ item, config }) => {
+
+        if (shouldSkip()) {
+            return;
+        }
 
         if ((item.resolveSourceLinks(LogicLinkKind.k8s).length == 0) &&
             (item.resolveTargetLinkItems(LogicLinkKind.svcaccount).length == 0))
@@ -15,5 +20,25 @@ export default K8sParser()
             item.raiseAlert(ValidatorID.UNUSED_SECRET, 'Secret not used.');
         }
 
+        /****/
+
+        function shouldSkip()
+        {
+            if (shouldSkipHelm()) {
+                return true;
+            }
+            return false;
+        }
+
+        function shouldSkipHelm()
+        {
+            if (config.type) {
+                if (config.type === 'helm.sh/release.v1') {
+                    return true;
+                }
+            }
+            return false;
+        }
+        
     })
     ;
