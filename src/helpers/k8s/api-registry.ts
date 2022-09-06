fee586e66168b351440fc16ce9438e3f15341625
Ruben Hakopian <ruben.hakopian@gmail.com>
2022-09-06 13:25:23 -0700

    Added K8sApiResourceStatusLoader

--- src/helpers/k8s/api-registry.ts
+++ src/helpers/k8s/api-registry.ts
@@ -1,6 +1,6 @@
-import { K8sApiResourceStatus } from '@kubevious/entity-meta';
 import _ from 'the-lodash';
 import { ILogger } from "the-logger";
+import { K8sApiResourceStatusConfig, K8sApiResourceStatusLoader } from '@kubevious/entity-meta';
 
 import { extractK8sConfigId, IConcreteRegistry, K8sConfig } from "../..";
 
@@ -12,7 +12,7 @@ export class K8sApiRegistry {
     private logger: ILogger;
     private _scope: LogicScope;
 
-    private _apiDict : { [apiVersion: string] : { [kind: string] : K8sApiResourceStatus }} = {};
+    private _loader? : K8sApiResourceStatusLoader;
     
     constructor(logger: ILogger, scope: LogicScope)
     {
@@ -31,29 +31,25 @@ export class K8sApiRegistry {
             return;
         }
 
-        const apiResources = apiResourceStatusObj.config?.resources as K8sApiResourceStatus[];
-        if (!apiResources) {
-            this.logger.error("[K8sApiRegistry::initialize] Empty ApiResourceStatus resources");
-            return;
-        }
-
-        for(const apiResource of apiResources)
+        const k8sApiResourceStatusConfig = (apiResourceStatusObj as any) as K8sApiResourceStatusConfig;
+        try
         {
-            if (!this._apiDict[apiResource.apiVersion]) {
-                this._apiDict[apiResource.apiVersion] = {};
-            }
-            this._apiDict[apiResource.apiVersion][apiResource.kindName] = apiResource;
+            this._loader = new K8sApiResourceStatusLoader(k8sApiResourceStatusConfig);
+        }
+        catch(reason)
+        {
+            this.logger.error("[K8sApiRegistry::initialize] ", reason);
         }
     }
 
     postProcessConfig(config: K8sConfig) : K8sConfig
     {
-        if (this._apiDict[config.apiVersion])
+        if (this._loader)
         {
-            const apiResource = this._apiDict[config.apiVersion][config.kind];
-            if (apiResource)
+            const resourceStatus = this._loader.getByApiVersionAndKind(config.apiVersion, config.kind);
+            if (resourceStatus)
             {
-                if (apiResource.isNamespaced)
+                if (resourceStatus.isNamespaced)
                 {
                     if (!config.metadata?.namespace)
                     {
